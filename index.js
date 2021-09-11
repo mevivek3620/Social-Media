5013cf89438d853ab8b658091745cf7b3c3adc84
Vivek Kumar <mevivek3620@gmail.com>
2021-09-11 18:27:47 +0530

    minor changes

--- index.js
+++ index.js
@@ -56,5 +56,6 @@ app.listen(port, function(err){
     if (err){
         console.log(`Error in running the server: ${err}`);
     }
-    console.log(`Server is running on port: ${port}`);
+    console.log(`Server is running on port number: ${port}`);
+
 });
